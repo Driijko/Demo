2bdf1ada43b7bccdfa28a3f5e17976c224eec3a9
Driijko <driijko@gmail.com>
2022-01-23 18:14:49 -0500

    Layout bug test 2.

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.addEventListener("load",function() {
   setTimeout(function(){
       // This hides the address bar:
       window.scrollTo(0, 1);
-      alert("test1");
+      alert("test2");
   }, 0);
 });
