fd81bc04a7f958ef0eae838492d94e0704a064fb
Driijko <driijko@gmail.com>
2022-01-25 19:52:52 -0500

    Layout test 9.

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// Redefine 'vh' in CSS based on what's actually available (excluding address bar).
 let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+// Determine what orientation device has.
+// let screenOrientation = null;
+// if (window.innerWidth < window.innerHeight) {
+//   screenOrientation = "portrait";
+// }
+// else {
+//   screenOrientation = "landscape";
+// }
+
+// if (orientation === "portrait") {
+
+// }
 let aspectRatio = window.innerHeight / window.innerWidth;
-// console.log(aspectRatio);
 if (aspectRatio < 1.777) {
   let width = ((0.5625 * window.innerHeight) / window.innerWidth) * window.innerWidth;
   document.documentElement.style.setProperty("--w", `${width}px`);
+  document.documentElement.style.setProperty("--gh", `${window.innerHeight}px`);
+  document.documentElement.style.setProperty("--gw", `${(window.innerWidth - width)/2}px`);
 }
 else {
   document.documentElement.style.setProperty("--w", `${window.innerWidth}px`);
+  document.documentElement.style.setProperty("--gh", `${(window.innerHeight - (window.innerWidth * 1.777))/2}px`);
+  document.documentElement.style.setProperty("--gw", `${window.innerWidth}px`);
 };
