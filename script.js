7fb29324806d4554822b3fe8bc576bd550921ca6
Driijko <driijko@gmail.com>
2022-04-02 15:53:04 -0400

    Test page load.

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-function layout(){let e=.01*window.innerHeight;if(document.documentElement.style.setProperty("--vh",`${e}px`),window.innerWidth<=window.innerHeight){if(window.innerHeight/window.innerWidth<1.777){let e=.5625*window.innerHeight/window.innerWidth*window.innerWidth;document.documentElement.style.setProperty("--w",`${e}px`),document.documentElement.style.setProperty("--h",`${window.innerHeight}px`),document.documentElement.style.setProperty("--gh",`${window.innerHeight+1}px`),document.documentElement.style.setProperty("--gw",`${(window.innerWidth-e)/2+1}px`)}else document.documentElement.style.setProperty("--h",`${1.777*window.innerWidth}px`),document.documentElement.style.setProperty("--w",`${window.innerWidth}px`),document.documentElement.style.setProperty("--gh",`${(window.innerHeight-1.777*window.innerWidth)/2+1}px`),document.documentElement.style.setProperty("--gw",`${window.innerWidth+1}px`)}else{if(window.innerWidth/window.innerHeight<1.6){let e=.625*window.innerWidth/window.innerHeight*window.innerHeight;document.documentElement.style.setProperty("--h",`${e}px`),document.documentElement.style.setProperty("--w",`${window.innerWidth}px`),document.documentElement.style.setProperty("--gh",`${(window.innerHeight-e)/2+1}px`),document.documentElement.style.setProperty("--gw",`${window.innerWidth+1}px`)}else{let t=100*e*1.6;document.documentElement.style.setProperty("--h",`${window.innerHeight}px`),document.documentElement.style.setProperty("--w",`${t}px`),document.documentElement.style.setProperty("--gh",`${window.innerHeight+1}px`),document.documentElement.style.setProperty("--gw",`${(window.innerWidth-t)/2+1}px`)}document.documentElement.style.setProperty("--orientation","landscape")}}function resize(){resizeReady=!0,layout()}layout();let resizeReady=!0;window.addEventListener("resize",()=>{resizeReady&&(resizeReady=!1,setTimeout(resize,2e3))});
+function layout () {
+  // Redefine 'vh' in CSS based on what's actually available (excluding address bar).
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  // Determine what orientation device has.
+  // We add +1 to gutter lengths to avoid layout bugs that I don't quite understand.
+  if (window.innerWidth <= window.innerHeight) {
+    let aspectRatio = window.innerHeight / window.innerWidth;
+    if (aspectRatio < 1.777) {
+      let width = ((0.5625 * window.innerHeight) / window.innerWidth) * window.innerWidth;
+      document.documentElement.style.setProperty("--w", `${width}px`);
+      document.documentElement.style.setProperty("--h", `${window.innerHeight}px`);
+      document.documentElement.style.setProperty("--gh", `${window.innerHeight + 1}px`);
+      document.documentElement.style.setProperty("--gw", `${((window.innerWidth - width)/2) + 1}px`);
+    }
+    else {
+      document.documentElement.style.setProperty("--h", `${window.innerWidth * 1.777}px`);
+      document.documentElement.style.setProperty("--w", `${window.innerWidth}px`);
+      document.documentElement.style.setProperty("--gh", `${((window.innerHeight - (window.innerWidth * 1.777))/2) + 1}px`);
+      document.documentElement.style.setProperty("--gw", `${window.innerWidth + 1}px`);
+    };
+    // document.documentElement.style.setProperty("--orientation", "portrait");
+  }
+  else {
+    let aspectRatio = window.innerWidth / window.innerHeight;
+    if(aspectRatio < 1.6) {
+      let height = ((0.625 * window.innerWidth) / window.innerHeight) * window.innerHeight;
+      document.documentElement.style.setProperty("--h", `${height}px`);
+      document.documentElement.style.setProperty("--w", `${window.innerWidth}px`);
+      document.documentElement.style.setProperty("--gh", `${((window.innerHeight - height)/2)+1}px`);
+      document.documentElement.style.setProperty("--gw", `${(window.innerWidth) + 1}px`);
+    }
+    else {
+      let width = vh * 100 * 1.6;
+      document.documentElement.style.setProperty("--h", `${window.innerHeight}px`);
+      document.documentElement.style.setProperty("--w", `${width}px`);
+      document.documentElement.style.setProperty("--gh", `${window.innerHeight + 1}px`);
+      document.documentElement.style.setProperty("--gw", `${((window.innerWidth - width)/2) + 1}px`);
+    }
+    document.documentElement.style.setProperty("--orientation", "landscape");
+  }
+}
+
+layout();
+
+function resize() {
+  resizeReady = true;
+  layout();
+}
+
+let resizeReady = true;
+window.addEventListener("resize", ()=> {
+  if(resizeReady) {
+    resizeReady = false;
+    setTimeout(resize, 2000);
+  }
+});
